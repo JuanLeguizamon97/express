68e4a7d5908a6de547ee2da8d023d295dac03d40
Juan Leguizamon <juanc.leguizamon@urosario.edu.co>
2024-08-27 09:53:06 -0500

    creation of the app and endpoints

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.send('Hola mi server en express');
+})
+
+app.get('/nuevaruta', (req, res)=>{
+    res.send('Hola soy una nueva ruta');
+});
+
+app.get('/products', (req, res)=>{
+    res.json({
+        name: 'Product1',
+        price:1000
+    });
+});
+
+
+app.listen(port, ()=>{
+    console.log('Mi port '+ port);
+});
