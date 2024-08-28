b54c1a8d2d971bb79aa64ea39e5a3e3fb4e1228e
Juan Leguizamon <juanc.leguizamon@urosario.edu.co>
2024-08-27 20:09:38 -0500

    routing creation

--- index.js
+++ index.js
@@ -13,37 +13,6 @@ app.get('/nuevaruta', (req, res)=>{
     res.send('Hola soy una nueva ruta');
 });
 
-app.get('/products', (req, res)=>{
-    const products = [];
-    const { size } = req.query;
-    const limit = size || 10;
-
-    for(let index = 0; index < limit; index++){
-        products.push({
-            name : faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(), 10),
-            image: faker.image.url()
-        });
-    }
-
-    res.json(products);
-});
-
-app.get('/products/filter', (req,res)=>{
-    res.send('yo soy un filter')
-})
-
-app.get('/products/:id',(req, res)=>{
-    const {id} = req.params;
-    res.json(
-        {
-            id,
-            name: 'Product2',
-            price:2000
-        }
-    );
-});
-
 app.get('/categories/:categoryId/products/:productId', (req, res)=>{
     const {categoryId, productId} = req.params;
     res.json({
