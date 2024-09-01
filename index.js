4ef387eadeb6870f46bb03d6f9b98a3da021c9e3
Juan Leguizamon <juanc.leguizamon@urosario.edu.co>
2024-09-01 17:48:34 -0500

    routing: categories, users and products

--- index.js
+++ index.js
@@ -5,29 +5,3 @@ const app = express();
 const port = 3000;
 
 routerApi(app);
-
-app.get('/categories/:categoryId/products/:productId', (req, res)=>{
-    const {categoryId, productId} = req.params;
-    res.json({
-        categoryId,
-        productId,
-        name: 'Product2',
-        price:2000
-    });
-});
-
-app.get('/users', (req,res)=>{
-    const {limit, offset} = req.query;
-    if(limit && offset){
-        res.json({
-            limit,
-            offset
-        });
-    }else{
-        res.send('No hat parametros');
-    }
-});
-
-app.listen(port, ()=>{
-    console.log('Mi port '+ port);
-});
