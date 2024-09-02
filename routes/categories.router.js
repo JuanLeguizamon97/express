6ebd2262ad5e204a6205604bc98e974493e7150d
Juan Leguizamon <juanc.leguizamon@urosario.edu.co>
2024-09-02 11:31:28 -0500

    Router modified

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/categories/:categoryId/products/:productId', (req, res)=>{
-    const {categoryId, productId} = req.params;
-    res.json({
-        categoryId,
-        productId,
-        name: 'Product2',
-        price:2000
-    });
-});
+router.get('/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+})
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
-const productsRouter = require('/products.router');
-const usersRouter = require('/users.router');
-const categoriesRouter = require('/categories.router');
+const express = require('express');
 
-function routerApi(app){
-    app.use('/products', productsRouter);
-    app.use('/users', usersRouter);
-    app.use('categories', categoriesRouter);
+const productsRouter = require('./products.router');
+const categoriesRouter = require('./categories.router');
+const usersRouter = require('./user.router');
+
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
 }
 
-module.export = routerApi;
+module.exports = routerApi;
